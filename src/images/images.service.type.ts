56beaf629d52da615faa31d92128469520fcf001
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-07-27 11:13:10 +0500

    update IImagesService

--- src/images/images.service.type.ts
+++ src/images/images.service.type.ts
@@ -17,7 +17,7 @@ export interface IImagesService {
     /** Name of decoder used to decompress image data e.g. jpeg, png, webp, gif, svg */
     format: string;
     /** Total size of image in bytes, for Stream and Buffer input only */
-    size: number;
+    size: number | undefined;
     /**  Number of pixels wide (EXIF orientation is not taken into consideration) */
     width: number;
     /** Number of pixels high (EXIF orientation is not taken into consideration) */
