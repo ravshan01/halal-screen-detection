b0ac5076960be0d3dcba02daf18de2b52814ccac
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-07-27 11:04:57 +0500

    create ImagesService

---
+++ src/images/images.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { IImagesService } from './images.service.type';
+import sharp from 'sharp';
+
+@Injectable()
+export class ImagesService implements IImagesService {
+  async getMetadata(
+    image:
+      | Buffer
+      | ArrayBuffer
+      | Uint8Array
+      | Uint8ClampedArray
+      | Int8Array
+      | Uint16Array
+      | Int16Array
+      | Uint32Array
+      | Int32Array
+      | Float32Array
+      | Float64Array
+      | string,
+  ) {
+    const metadata = await sharp(image).metadata();
+
+    return {
+      format: metadata.format,
+      size: metadata.size,
+      width: metadata.width,
+      height: metadata.height,
+    };
+  }
+}
