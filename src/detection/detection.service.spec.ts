7dd35e97551229fbbef21ceb53676b95b6984dbc
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-07-28 15:53:07 +0500

    implemented DetectionService tests

--- src/detection/detection.service.spec.ts
+++ src/detection/detection.service.spec.ts
@@ -1,11 +1,13 @@
 import * as fs from 'node:fs';
-import { join } from 'node:path';
 
 import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { ImagesModule } from '../images/images.module';
 import { DetectionService } from './detection.service';
+import { DetectionObject } from './enums/detection-object.enum';
+import { DETECTION_IMAGES_WITH_RESULT_FOR_TEST } from './mock/images';
+import { IDetection } from './types/detection.types';
 
 describe('DetectionService', () => {
   let service: DetectionService;
@@ -28,12 +30,34 @@ describe('DetectionService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should detect labels in an image', async () => {
-    const buffer = await fs.promises.readFile(
-      join(__dirname, './mock/images/room.jpg'),
-    );
-    const labels = await service.detectLabelsInImage(buffer);
+  it("'detectLabelsInImage' should detect labels in an image", async () => {
+    const detectionImageWithResult = DETECTION_IMAGES_WITH_RESULT_FOR_TEST[3];
 
-    expect(labels).toBeDefined();
+    const buffer = await fs.promises.readFile(detectionImageWithResult.path);
+    const detections = await service.detectLabelsInImage(buffer);
+
+    expect(detections).toBeDefined();
+    expect(detections).toBeInstanceOf(Array);
+
+    const groupedDetections = detections.reduce(
+      (acc, detection) => ({
+        ...acc,
+        [detection.object]: [...(acc[detection.object] || []), detection],
+      }),
+      {} as Record<DetectionObject, IDetection[]>,
+    );
+
+    Object.entries(groupedDetections).forEach(([object, detections]) => {
+      const count =
+        detectionImageWithResult.result[object as DetectionObject].count;
+
+      if (typeof count === 'number') {
+        expect(detections).toHaveLength(count);
+      }
+      if (typeof count === 'object') {
+        expect(detections.length).toBeGreaterThanOrEqual(count.min);
+        expect(detections.length).toBeLessThanOrEqual(count.max);
+      }
+    });
   });
 });
