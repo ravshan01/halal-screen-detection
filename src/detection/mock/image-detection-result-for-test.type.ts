ed28b72da9cf1f9167238265eba66946b87cd3d1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-07-28 15:30:49 +0500

    update IImageDetectionResultForTest count

--- src/detection/mock/image-detection-result-for-test.type.ts
+++ src/detection/mock/image-detection-result-for-test.type.ts
@@ -3,6 +3,6 @@ import { DetectionObject } from '../enums/detection-object.enum';
 export type IImageDetectionResultForTest = Record<
   DetectionObject,
   {
-    count: number;
+    count: number | { min: number; max: number };
   }
 >;
