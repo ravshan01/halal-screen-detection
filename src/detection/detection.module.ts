19c3b3da1e6a568e6bfe6f492cbf532af455c9ef
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-08-12 18:59:55 +0500

    DetectionModule use DetectionController

--- src/detection/detection.module.ts
+++ src/detection/detection.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { ImagesModule } from '../images/images.module';
 import { DETECTION_SERVICE_KEY } from './constants/di-keys.constants';
+import { DetectionController } from './detection.controller';
 import { DetectionService } from './detection.service';
 
 @Module({
   imports: [ImagesModule],
+  controllers: [DetectionController],
   providers: [
     {
       provide: DETECTION_SERVICE_KEY,
       useClass: DetectionService,
     },
   ],
-  controllers: [],
 })
 export class DetectionModule {}
