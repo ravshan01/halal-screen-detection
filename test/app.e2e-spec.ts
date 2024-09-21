64b680d7d644848aa9ec210ae970171647a14b79
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-21 17:45:48 +0500

    implemented app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,82 @@
+import * as fs from 'node:fs';
+import { join } from 'node:path';
+import * as process from 'node:process';
+
+import { firstValueFrom, Observable } from 'rxjs';
+
+import { INestMicroservice } from '@nestjs/common';
+import { ClientGrpc, ClientsModule, Transport } from '@nestjs/microservices';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+
+import { AppModule } from '../src/app.module';
+import {
+  type DetectImagesRequest as IDetectImagesRequest,
+  DetectImagesRequest,
+  type DetectImagesResponse as IDetectImagesResponse,
+  Image,
+} from '../src/proto/detection';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let microservice: INestMicroservice;
+  let detectionService: {
+    DetectImages: (
+      request: IDetectImagesRequest,
+    ) => Observable<IDetectImagesResponse>;
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        AppModule,
+        ClientsModule.register([
+          {
+            name: 'DETECTION_PACKAGE',
+            transport: Transport.GRPC,
+            options: {
+              package: 'detection',
+              protoPath: join(process.cwd(), 'src/proto/detection.proto'),
+            },
+          },
+        ]),
+      ],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    microservice = moduleFixture.createNestMicroservice({
+      transport: Transport.GRPC,
+      options: {
+        package: 'detection',
+        protoPath: join(process.cwd(), 'src/proto/detection.proto'),
+      },
+    });
+    await microservice.listen();
+
+    const detectionClient: ClientGrpc = microservice.get('DETECTION_PACKAGE');
+    detectionService = detectionClient.getService('DetectionService');
+  });
+  afterEach(async () => {
+    await microservice.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('AppController.DetectImages', () => {
+    it('should detect images and return detections', async () => {
+      const image = await fs.promises.readFile(
+        join(process.cwd(), 'src/images/mock/image.jpg'),
+      );
+
+      const observable = detectionService.DetectImages(
+        DetectImagesRequest.create({
+          images: [Image.create({ content: image })],
+        }),
+      );
+      const data = await firstValueFrom(observable);
+
+      expect(data).toBeDefined();
+      expect(data.detections).toBeDefined();
+      expect(data.detections.length).toBe(1);
+    });
+
+    it.todo(
+      'should return an error if a file of a different format was transferred under the images',
+    );
   });
 });
