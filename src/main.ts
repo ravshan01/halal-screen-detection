a376a6c8be7c97e4a89a70936560e0afe2e21a43
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-22 08:26:28 +0500

    update bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,25 @@
 import { join } from 'node:path';
 
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
+import { IEnvironmentVariables } from './config/types/environment-variables.type';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        package: 'detection',
-        protoPath: join(__dirname, 'proto/detection.proto'),
-      },
-    },
-  );
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService<IEnvironmentVariables>);
 
-  await app.listen();
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'detection',
+      protoPath: join(__dirname, 'proto/detection.proto'),
+      url: `localhost:${configService.get<number>('PORT')}`,
+    },
+  });
+
+  await app.startAllMicroservices();
 }
 bootstrap();
