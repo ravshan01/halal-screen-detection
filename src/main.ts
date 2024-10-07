84913d5a835a5f9ab1d8f3b9875ee7e81c28c30f
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-10-07 07:25:58 +0500

    style commit

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,14 @@ import { IEnvVariables } from './config/types/env-variables.type';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<IEnvVariables>);
+  const port = configService.get<number>('PORT');
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       package: 'detection',
       protoPath: join(__dirname, 'proto/detection.proto'),
-      url: `localhost:${configService.get<number>('PORT')}`,
+      url: `localhost:${port}`,
     },
   });
 
